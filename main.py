cf53d1db75f6702c9a08edf1336f6751c96aa253
Chris Nosowsky <ChrisNosowsky@gmail.com>
2021-11-25 02:00:03 -0500

    Initial project files setup. Added gitignore. Added main.py file to run our application.

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# ==============================================================================
+# CSE 824
+# Created on: November 25th, 2021
+# Authors: Chris Nosowsky (nosowsky@msu.edu),
+#          Hamzeh Alzweri (alzwerih@msu.edu)
+#
+# Main script to run our application
+# ==============================================================================
+
+if __name__ == '__main__':
+    print("Running Cellular Network Trace Application...")
